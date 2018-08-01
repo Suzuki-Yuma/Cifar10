3c1138640ccca0a08ccf7823b3c861a7f87eca82
yuma_suzuki <s.yuma.2990.mawasi@gmail.com>
2018-08-01 09:59:47 +0900

    [modify]

--- chainer_cnn_cifar10.py
+++ chainer_cnn_cifar10.py
@@ -93,7 +93,7 @@ def KL_loss(y, t):
 def cos_sim(y, t):
     y_ = chainer.Variable(xp.eye(10).astype(xp.float32))[
         chainer.cuda.to_cpu(chainer.functions.argmax(y, axis=1).data)]
-    return chainer.functions.mean(chainer.functions.sum(y_ * t, axis=1) / chainer.functions.batch_l2_norm_squared(t) / chainer.functions.batch_l2_norm_squared(y_))
+    return chainer.functions.mean(chainer.functions.sum(y_ * t, axis=1) / chainer.functions.sqrt(chainer.functions.batch_l2_norm_squared(t) * chainer.functions.batch_l2_norm_squared(y_)))
 
 
 def main():
